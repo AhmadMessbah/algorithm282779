5dc5d1da9ccbcd6383bdeb611f23bba106bc5fa3
hessambahraminia <hessambhm@gmail.com>
2023-09-04 20:18:33 +0430

    hessam bahraminia . edited bubble_sort.py

--- model/bubble_sort.py
+++ model/bubble_sort.py
@@ -1,2 +1,12 @@
 print("Salam",1)
 x = [1, 7, 2, 6, 12]
+n = len(x)
+
+for i in range(n):
+
+    for j in range(0, n - i - 1):
+
+        if x[j] > x[j + 1]:
+                x[j], x[j + 1] = x[j + 1], x[j]
+
+print("Sorted array is:", x)
